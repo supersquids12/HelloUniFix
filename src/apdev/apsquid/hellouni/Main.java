0288b032a1667c657324f35b3ffe5e58eec93a87
supersquids12 <34383703+supersquids12@users.noreply.github.com>
2018-04-30 22:52:10 -0400

    initial commit
    added helloUni(said hi to uni)

---
+++ src/apdev/apsquid/hellouni/Main.java
@@ -0,0 +1,37 @@
+package apdev.apsquid.hellouni;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if(command.getName().toLowerCase().equals("hellouni")) {
+			
+			sender.sendMessage("Uni says Hello. Now Fuck Off.");
+			
+		if(command.getName().toLowerCase().equals("helloccm")) {
+				
+				sender.sendMessage("Ccm says Hi. Now make your code formated.");
+				
+		if(command.getName().toLowerCase().equals("hellosquid")) {
+					
+					sender.sendMessage("Rawr.");
+					
+	}
+	
+	return false;
+	
+}
+}
