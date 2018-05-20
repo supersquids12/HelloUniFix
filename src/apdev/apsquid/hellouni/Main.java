09d235757f2ec5a1536dd3d8ea549bff2749ed36
Squiddo <34383703+supersquids12@users.noreply.github.com>
2018-05-20 17:04:31 -0400

    please done
    I hope this is the last fixing commit ;-;

--- src/apdev/apsquid/hellouni/Main.java
+++ src/apdev/apsquid/hellouni/Main.java
@@ -6,63 +6,62 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
 
 	public void onEnable() {
-		
+
 	}
-	
+
 	public void onDisable() {
-		
+
 	}
-	
-	
+
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		
-		if(command.getName().toLowerCase().equals("hellouni")) {
-			
+
+		if (command.getName().equalsIgnoreCase("hellouni")) {
+
 			sender.sendMessage("Uni says Hello. Now Fuck Off.");
 			return true;
-		}	
-		if(command.getName().toLowerCase().equals("helloccm")) {
-				
+		}
+		if (command.getName().equalsIgnoreCase("helloccm")) {
+
 			sender.sendMessage("Ccm says Hi. Now make your code formated.");
 			return true;
 		}
-		if(command.getName().toLowerCase().equals("hellosquid")) {
-					
+		if (command.getName().equalsIgnoreCase("hellosquid")) {
+
 			sender.sendMessage("Rawr.");
 			return true;
 		}
-		
-        	if(command.getName().toLowerCase().equals("hello")) {
-        	
-        		if (args.length < 1) {
-    				sender.sendMessage("Use either \\'/hello spam\\' or \\'/hello random\\'");
-    				return true;
-        		}
-        	
-        		if (args[0].equalsIgnoreCase("spam")) {
-        			for(int x = 0; x < 30; x++) {
-				sender.sendMessage("Hello");
-        	    		}
+
+		if (command.getName().equalsIgnoreCase("hello")) {
+
+			if (args.length < 1) {
+				sender.sendMessage("Use either \'/hello spam\' or \'/hello random\'");
 				return true;
-        		}
-        		ArrayList<String> responses = new ArrayList<String>();
-        		responses.add("Rawr");
-        		responses.add("Ccm says Hi. Now make your code formatted.");
-        		responses.add("Uni says hello. Now Fuck Off.");
-		
-        		if (args[0].equalsIgnoreCase("random")) {
-			
-        			int min = 0, max = responses.size();
-        			int randomNum = ThreadLocalRandom.current().nextInt(min, max);
-        			sender.sendMessage(responses.get(randomNum));
-        			return true;
-        			}
-        		sender.sendMessage("Use either \'/hello spam\' or \'/hello random\'");
+			}
+
+			if (args[0].equalsIgnoreCase("spam")) {
+				for (int x = 0; x < 30; x++) {
+					sender.sendMessage("Hello");
+				}
 				return true;
-        	}
-        	return false;
+			}
+			ArrayList<String> responses = new ArrayList<String>();
+			responses.add("Rawr");
+			responses.add("Ccm says Hi. Now make your code formatted.");
+			responses.add("Uni says hello. Now Fuck Off.");
+
+			if (args[0].equalsIgnoreCase("random")) {
+
+				int min = 0, max = responses.size();
+				int randomNum = ThreadLocalRandom.current().nextInt(min, max);
+				sender.sendMessage(responses.get(randomNum));
+				return true;
+			}
+			sender.sendMessage("Use either \'/hello spam\' or \'/hello random\'");
+			return true;
+		}
+		return false;
 	}
 }
