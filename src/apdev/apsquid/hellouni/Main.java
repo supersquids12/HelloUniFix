df1e05ea1232fddaa0905bbb452f849a227cad3a
Squiddo <34383703+supersquids12@users.noreply.github.com>
2018-05-20 16:46:37 -0400

    more syntax
    I hate cc

--- src/apdev/apsquid/hellouni/Main.java
+++ src/apdev/apsquid/hellouni/Main.java
@@ -35,34 +35,34 @@ public class Main extends JavaPlugin{
 			return true;
 		}
 		
-        if(command.getName().toLowerCase().equals("hello")) {
+        	if(command.getName().toLowerCase().equals("hello")) {
         	
-        	if (args.length < 2) {
-    			sender.sendMessage("Use either \\'/hello spam\\' or \\'/hello random\\'");
-    			return true;
-        	}
-        	
-        	if (args[0].equalsIgnoreCase("spam")) {
-        		for(int x = 0; x < 30; x++) {
-				sender.sendMessage("Hello");
-        	    	}
-			return true;
-        	}
-        	ArrayList<String> responses = new ArrayList<String>();
-        	responses.add("Rawr");
-        	responses.add("Ccm says Hi. Now make your code formated.");
-        	responses.add("Uni says hello. Now Fuck Off.");
-		
-        	if (args[0].equalsIgnoreCase("random")) {
-			
-        		int min = 0, max = responses.size();
-        		int randomNum = ThreadLocalRandom.current().nextInt(min, max);
-        		sender.sendMessage(responses.get(randomNum));
-        		return true;
+        		if (args.length < 2) {
+    				sender.sendMessage("Use either \\'/hello spam\\' or \\'/hello random\\'");
+    				return true;
         		}
-        	sender.sendMessage("Use either \'/hello spam\' or \'/hello random\'");
-			return true;
-        }
-        return false;
+        	
+        		if (args[0].equalsIgnoreCase("spam")) {
+        			for(int x = 0; x < 30; x++) {
+				sender.sendMessage("Hello");
+        	    		}
+				return true;
+        		}
+        		ArrayList<String> responses = new ArrayList<String>();
+        		responses.add("Rawr");
+        		responses.add("Ccm says Hi. Now make your code formated.");
+        		responses.add("Uni says hello. Now Fuck Off.");
+		
+        		if (args[0].equalsIgnoreCase("random")) {
+			
+        			int min = 0, max = responses.size();
+        			int randomNum = ThreadLocalRandom.current().nextInt(min, max);
+        			sender.sendMessage(responses.get(randomNum));
+        			return true;
+        			}
+        		sender.sendMessage("Use either \'/hello spam\' or \'/hello random\'");
+				return true;
+        	}
+        	return false;
 	}
 }
